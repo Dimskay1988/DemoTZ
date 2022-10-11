be8ae0a2daf385b211eee4febb47d95aa5cc9db1
anikeenko <dimskay1988@icloud.com>
2022-10-11 16:05:09 +0300

    add models Catalog

--- apps/Base/models.py
+++ apps/Base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Catalog(models.Model):
+    article = models.IntegerField()
+    brand = models.CharField(max_length=60, null=True, blank=True)
+    title = models.CharField(max_length=350, null=True, blank=True)
+
+    def __str__(self):
+        return self.article
